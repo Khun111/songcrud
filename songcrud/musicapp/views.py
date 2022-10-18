273d3cfb0cebfecfe45c5392337c64a93481cfd1
Oluwatobi <amure387@gmail.com>
2022-10-18 14:25:11 +0100

    finally*

--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world. Add some issues with git in virtual env but voila')
+    return HttpResponse('Hello, world. Had some issues with git in virtual env but voila')
