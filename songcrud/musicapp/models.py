a5acc8c8373a599a6eadc4eec395eb529ea6b965
Oluwatobi <amure387@gmail.com>
2022-10-20 18:30:08 +0100

    models created

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
