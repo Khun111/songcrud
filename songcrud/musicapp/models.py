5a5f6faaea804076ed00564b7256a318dbdbdb37
Oluwatobi <amure387@gmail.com>
2022-10-20 21:00:12 +0100

    push

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -5,12 +5,18 @@ class Artiste(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     age = models.IntegerField()
+    def __str__(self):
+        return self.first_name
 
 class Song(models.Model):
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     date_released = models.DateField(default=datetime.today)
     likes = models.IntegerField()
+    def __str__(self):
+        return self.artiste_id
 
 class Lyric(models.Model):
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.content
